625846b81d9a11405cbff986074eda59512b98b3
Jan Gosmann <jan@hyper-world.de>
2020-12-27 12:34:01 +0100

    Implement PrometheusExporter

---
+++ dmarc_metrics_exporter/prometheus_exporter.py
@@ -0,0 +1,116 @@
+import threading
+from typing import Tuple, cast
+
+import uvicorn
+from prometheus_client.core import REGISTRY, CounterMetricFamily
+from prometheus_client.exposition import make_asgi_app
+
+from dmarc_metrics_exporter.dmarc_event import Disposition, Meta
+from dmarc_metrics_exporter.dmarc_metrics import DmarcMetricsCollection
+
+
+class Server:
+    def __init__(self, exporter: "PrometheusExporter", port=9119):
+        self.exporter = exporter
+        config = uvicorn.Config(make_asgi_app(), port=port)
+        self.server = uvicorn.Server(config)
+        self.host = config.host
+        self.port = port
+        self._main_loop = None
+
+    async def __aenter__(self):
+        REGISTRY.register(self.exporter)
+        config = self.server.config
+        if not config.loaded:
+            config.load()
+        self.server.lifespan = config.lifespan_class(config)
+        await self.server.startup()
+        self._main_loop = self.server.main_loop()
+        return self
+
+    async def __aexit__(self, exc_type, exc, traceback):
+        self.server.should_exit = True
+        await self._main_loop
+        self._main_loop = None
+        await self.server.shutdown()
+        REGISTRY.unregister(self.exporter)
+
+
+class PrometheusExporter:
+    LABELS = ("reporter", "from_domain", "dkim_domain", "spf_domain")
+
+    def __init__(self, metrics: DmarcMetricsCollection):
+        self._metrics_lock = threading.Lock()
+        self._metrics = metrics
+
+    def start_server(self):
+        return Server(self)
+
+    def collect(self):
+        dmarc_total = CounterMetricFamily(
+            "dmarc_total", "Total number of reported messages.", labels=self.LABELS
+        )
+        dmarc_compliant_total = CounterMetricFamily(
+            "dmarc_compliant_total",
+            "Total number of DMARC compliant messages.",
+            labels=self.LABELS,
+        )
+        dmarc_quarantine_total = CounterMetricFamily(
+            "dmarc_quarantine_total",
+            "Total number of quarantined messages.",
+            labels=self.LABELS,
+        )
+        dmarc_reject_total = CounterMetricFamily(
+            "dmarc_reject_total",
+            "Total number of rejected messages.",
+            labels=self.LABELS,
+        )
+        dmarc_spf_aligned_total = CounterMetricFamily(
+            "dmarc_spf_aligned_total",
+            "Total number of SPF algined messages.",
+            labels=self.LABELS,
+        )
+        dmarc_spf_pass_total = CounterMetricFamily(
+            "dmarc_spf_pass_total",
+            "Total number of messages with raw SPF pass.",
+            labels=self.LABELS,
+        )
+        dmarc_dkim_aligned_total = CounterMetricFamily(
+            "dmarc_dkim_aligned_total",
+            "Total number of DKIM algined messages.",
+            labels=self.LABELS,
+        )
+        dmarc_dkim_pass_total = CounterMetricFamily(
+            "dmarc_dkim_pass_total",
+            "Total number of messages with raw DKIM pass.",
+            labels=self.LABELS,
+        )
+        with self._metrics_lock:
+            for meta, metrics in self._metrics.items():
+                labels = self._meta2labels(meta)
+                dmarc_total.add_metric(labels, metrics.total_count)
+                dmarc_compliant_total.add_metric(labels, metrics.dmarc_compliant_count)
+                dmarc_quarantine_total.add_metric(
+                    labels, metrics.disposition_counts.get(Disposition.QUARANTINE, 0)
+                )
+                dmarc_reject_total.add_metric(
+                    labels, metrics.disposition_counts.get(Disposition.REJECT, 0)
+                )
+                dmarc_spf_aligned_total.add_metric(labels, metrics.spf_aligned_count)
+                dmarc_spf_pass_total.add_metric(labels, metrics.spf_pass_count)
+                dmarc_dkim_aligned_total.add_metric(labels, metrics.dkim_aligned_count)
+                dmarc_dkim_pass_total.add_metric(labels, metrics.dkim_pass_count)
+        return (
+            dmarc_total,
+            dmarc_compliant_total,
+            dmarc_quarantine_total,
+            dmarc_reject_total,
+            dmarc_spf_aligned_total,
+            dmarc_spf_pass_total,
+            dmarc_dkim_aligned_total,
+            dmarc_dkim_pass_total,
+        )
+
+    @classmethod
+    def _meta2labels(cls, meta: Meta) -> Tuple[str]:
+        return cast(Tuple[str], tuple(getattr(meta, label) for label in cls.LABELS))
---
+++ dmarc_metrics_exporter/tests/test_prometheus_exporter.py
@@ -0,0 +1,85 @@
+import dataclasses
+
+import aiohttp
+import pytest
+from prometheus_client.parser import text_string_to_metric_families
+from prometheus_client.samples import Sample
+
+from dmarc_metrics_exporter.dmarc_event import Disposition, Meta
+from dmarc_metrics_exporter.dmarc_metrics import DmarcMetrics, DmarcMetricsCollection
+from dmarc_metrics_exporter.prometheus_exporter import PrometheusExporter
+
+
+@pytest.mark.asyncio
+async def test_prometheus_exporter():
+    metrics = DmarcMetricsCollection(
+        metrics={
+            Meta(
+                reporter="google.com",
+                from_domain="mydomain.de",
+                dkim_domain="sub.mydomain.de",
+                spf_domain="mydomain.de",
+            ): DmarcMetrics(
+                total_count=42,
+                disposition_counts={
+                    Disposition.QUARANTINE: 3,
+                    Disposition.NONE_VALUE: 39,
+                },
+                dmarc_compliant_count=39,
+                dkim_aligned_count=39,
+                dkim_pass_count=39,
+                spf_pass_count=42,
+                spf_aligned_count=42,
+            ),
+            Meta(
+                reporter="yahoo.com",
+                from_domain="mydomain.de",
+                dkim_domain="sub.mydomain.de",
+                spf_domain="mydomain.de",
+            ): DmarcMetrics(
+                total_count=1,
+                disposition_counts={Disposition.NONE_VALUE: 1},
+                dmarc_compliant_count=1,
+                dkim_aligned_count=1,
+                dkim_pass_count=1,
+                spf_pass_count=1,
+                spf_aligned_count=1,
+            ),
+        }
+    )
+
+    exporter = PrometheusExporter(metrics)
+    async with exporter.start_server() as server:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"http://{server.host}:{server.port}/metrics"
+            ) as response:
+                served_metrics = text_string_to_metric_families(await response.text())
+
+    samples = [
+        sample for served_metric in served_metrics for sample in served_metric.samples
+    ]
+    expected_metrics = {
+        "dmarc_total": lambda m: m.total_count,
+        "dmarc_compliant_total": lambda m: m.dmarc_compliant_count,
+        "dmarc_quarantine_total": lambda m: m.disposition_counts.get(
+            Disposition.QUARANTINE, 0
+        ),
+        "dmarc_reject_total": lambda m: m.disposition_counts.get(Disposition.REJECT, 0),
+        "dmarc_dkim_aligned_total": lambda m: m.dkim_aligned_count,
+        "dmarc_dkim_pass_total": lambda m: m.dkim_pass_count,
+        "dmarc_spf_aligned_total": lambda m: m.spf_aligned_count,
+        "dmarc_spf_pass_total": lambda m: m.spf_pass_count,
+    }
+    for meta, metric in metrics.items():
+        for prometheus_name, getter in expected_metrics.items():
+            assert (
+                Sample(
+                    prometheus_name,
+                    labels=dataclasses.asdict(meta),
+                    value=getter(metric),
+                    timestamp=None,
+                    exemplar=None,
+                )
+                in samples
+            )
