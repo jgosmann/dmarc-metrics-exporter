967b8340ad50295ec0fe2871e1486512198cc93c
Jan Gosmann <jan@hyper-world.de>
2020-12-28 16:50:43 +0100

    Implement conversion to event

--- dmarc_metrics_exporter/deserialization.py
+++ dmarc_metrics_exporter/deserialization.py
@@ -1,13 +1,25 @@
 import io
 from email.contentmanager import raw_data_manager
 from email.message import EmailMessage
-from typing import Generator
+from typing import Generator, Optional
 from zipfile import ZipFile
 
 from xsdata.formats.dataclass.context import XmlContext
 from xsdata.formats.dataclass.parsers.xml import XmlParser
 
-from dmarc_metrics_exporter.model.dmarc_aggregate_report import Feedback
+from dmarc_metrics_exporter.dmarc_event import (
+    Disposition,
+    DmarcEvent,
+    DmarcResult,
+    Meta,
+)
+from dmarc_metrics_exporter.model.dmarc_aggregate_report import (
+    DispositionType,
+    DkimresultType,
+    DmarcresultType,
+    Feedback,
+    SpfresultType,
+)
 
 
 def _get_payloads_from_zip(zip_bytes: bytes) -> Generator[str, None, None]:
@@ -29,3 +41,73 @@ def get_aggregate_report_from_email(
             content = raw_data_manager.get_content(part)
             for payload in _get_payloads_from_zip(content):
                 yield parser.from_string(payload, Feedback)
+
+
+def _map_disposition(disposition: Optional[DispositionType]) -> Disposition:
+    if disposition is None:
+        return Disposition.NONE_VALUE
+    return {
+        DispositionType.NONE_VALUE: Disposition.NONE_VALUE,
+        DispositionType.QUARANTINE: Disposition.QUARANTINE,
+        DispositionType.REJECT: Disposition.REJECT,
+    }[disposition]
+
+
+def convert_to_events(feedback: Feedback) -> Generator[DmarcEvent, None, None]:
+    for record in feedback.record:
+        if record.row is None:
+            continue
+
+        if feedback.report_metadata:
+            reporter = feedback.report_metadata.org_name or ""
+        else:
+            reporter = ""
+
+        if record.identifiers:
+            from_domain = record.identifiers.header_from or ""
+        else:
+            from_domain = ""
+
+        if record.auth_results and len(record.auth_results.dkim) > 0:
+            dkim = record.auth_results.dkim[0]
+            dkim_domain = dkim.domain or ""
+            dkim_pass = dkim.result == DkimresultType.PASS_VALUE
+        else:
+            dkim_domain = ""
+            dkim_pass = False
+
+        if record.auth_results and len(record.auth_results.spf) > 0:
+            spf = record.auth_results.spf[0]
+            spf_domain = spf.domain or ""
+            spf_pass = spf.result == SpfresultType.PASS_VALUE
+        else:
+            spf_domain = ""
+            spf_pass = False
+
+        if record.row.policy_evaluated:
+            disposition = _map_disposition(record.row.policy_evaluated.disposition)
+            dkim_aligned = (
+                record.row.policy_evaluated.dkim == DmarcresultType.PASS_VALUE
+            )
+            spf_aligned = record.row.policy_evaluated.spf == DmarcresultType.PASS_VALUE
+        else:
+            disposition = Disposition.NONE_VALUE
+            dkim_aligned = False
+            spf_aligned = False
+
+        yield DmarcEvent(
+            count=record.row.count or 1,
+            meta=Meta(
+                reporter=reporter,
+                from_domain=from_domain,
+                dkim_domain=dkim_domain,
+                spf_domain=spf_domain,
+            ),
+            result=DmarcResult(
+                disposition=disposition,
+                dkim_aligned=dkim_aligned,
+                spf_aligned=spf_aligned,
+                dkim_pass=dkim_pass,
+                spf_pass=spf_pass,
+            ),
+        )
--- dmarc_metrics_exporter/tests/test_deserialization.py
+++ dmarc_metrics_exporter/tests/test_deserialization.py
@@ -4,7 +4,16 @@ from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 from zipfile import ZipFile
 
-from dmarc_metrics_exporter.deserialization import get_aggregate_report_from_email
+from dmarc_metrics_exporter.deserialization import (
+    convert_to_events,
+    get_aggregate_report_from_email,
+)
+from dmarc_metrics_exporter.dmarc_event import (
+    Disposition,
+    DmarcEvent,
+    DmarcResult,
+    Meta,
+)
 from dmarc_metrics_exporter.model.tests.sample_data import SAMPLE_DATACLASS, SAMPLE_XML
 
 
@@ -38,3 +47,24 @@ def test_extracts_zipped_xml_from_email():
     msg.add_attachment(xml)
 
     assert list(get_aggregate_report_from_email(msg)) == [SAMPLE_DATACLASS]
+
+
+def test_convert_to_events():
+    assert list(convert_to_events(SAMPLE_DATACLASS)) == [
+        DmarcEvent(
+            count=1,
+            meta=Meta(
+                reporter="google.com",
+                from_domain="mydomain.de",
+                dkim_domain="mydomain.de",
+                spf_domain="my-spf-domain.de",
+            ),
+            result=DmarcResult(
+                disposition=Disposition.NONE_VALUE,
+                dkim_pass=True,
+                dkim_aligned=True,
+                spf_pass=True,
+                spf_aligned=False,
+            ),
+        )
+    ]
