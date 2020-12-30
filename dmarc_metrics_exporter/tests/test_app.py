521f0097d8933a114d91f916f30f48d338520b91
Jan Gosmann <jan@hyper-world.de>
2020-12-30 20:37:38 +0100

    Reduce code duplication in test_app.py

--- dmarc_metrics_exporter/tests/test_app.py
+++ dmarc_metrics_exporter/tests/test_app.py
@@ -1,4 +1,6 @@
 import asyncio
+from dataclasses import dataclass, field, fields
+from typing import Tuple
 from unittest.mock import MagicMock
 
 import pytest
@@ -21,65 +23,58 @@ async def async_noop():
     pass
 
 
+@dataclass
+class AppDependencies:
+    prometheus_addr: Tuple[str, int] = ("127.0.0.1", 9119)
+    exporter_cls: MagicMock = field(default_factory=MagicMock)
+    metrics_persister: MagicMock = field(default_factory=MagicMock)
+    imap_queue: MagicMock = field(default_factory=MagicMock)
+
+    def as_flat_dict(self):
+        return {field.name: getattr(self, field.name) for field in fields(self)}
+
+
+@dataclass
+class AppMocks:
+    dependencies: AppDependencies = field(default_factory=AppDependencies)
+    metrics: DmarcMetricsCollection = field(default_factory=DmarcMetricsCollection)
+    metrics_provider: MagicMock = field(default_factory=MagicMock)
+    exporter: MagicMock = field(default_factory=MagicMock)
+
+    def __post_init__(self):
+        self.metrics_provider.__enter__.return_value = self.metrics
+        self.exporter.start_server.return_value = ServerMock()
+        self.exporter.get_metrics.return_value = self.metrics_provider
+        self.dependencies.exporter_cls.return_value = self.exporter
+        self.dependencies.metrics_persister.load.return_value = self.metrics
+        self.dependencies.imap_queue.stop_consumer.side_effect = async_noop
+
+
 @pytest.mark.asyncio
 async def test_loads_persisted_metrics_and_stores_them_on_shutdown():
-    metrics = DmarcMetricsCollection()
-    metrics_provider = MagicMock()
-    metrics_provider.__enter__.return_value = metrics
-    exporter = MagicMock()
-    exporter.start_server.return_value = ServerMock()
-    exporter.get_metrics.return_value = metrics_provider
-    exporter_cls = MagicMock()
-    exporter_cls.return_value = exporter
-    metrics_persister = MagicMock()
-    metrics_persister.load.return_value = metrics
-    imap_queue = MagicMock()
-    imap_queue.stop_consumer.return_value = async_noop()
-    app = App(
-        prometheus_addr=("127.0.0.1", 9119),
-        imap_queue=imap_queue,
-        exporter_cls=exporter_cls,
-        metrics_persister=metrics_persister,
-        autosave_interval_seconds=None,
-    )
+    mocks = AppMocks()
+    app = App(autosave_interval_seconds=None, **mocks.dependencies.as_flat_dict())
     main = asyncio.create_task(app.run())
 
     try:
         await try_until_success(
-            metrics_persister.load.assert_called_once, timeout_seconds=2
+            app.metrics_persister.load.assert_called_once, timeout_seconds=2
         )
     finally:
         main.cancel()
         await main
-    metrics_persister.save.assert_called_once_with(metrics)
+    mocks.dependencies.metrics_persister.save.assert_called_once_with(mocks.metrics)
 
 
 @pytest.mark.asyncio
 async def test_metrics_autosave():
-    metrics = DmarcMetricsCollection()
-    metrics_provider = MagicMock()
-    metrics_provider.__enter__.return_value = metrics
-    exporter = MagicMock()
-    exporter.start_server.return_value = ServerMock()
-    exporter.get_metrics.return_value = metrics_provider
-    exporter_cls = MagicMock()
-    exporter_cls.return_value = exporter
-    metrics_persister = MagicMock()
-    metrics_persister.load.return_value = metrics
-    imap_queue = MagicMock()
-    imap_queue.stop_consumer.return_value = async_noop()
-    app = App(
-        prometheus_addr=("127.0.0.1", 9119),
-        imap_queue=imap_queue,
-        exporter_cls=exporter_cls,
-        metrics_persister=metrics_persister,
-        autosave_interval_seconds=0.5,
-    )
+    mocks = AppMocks()
+    app = App(autosave_interval_seconds=0.5, **mocks.dependencies.as_flat_dict())
     main = asyncio.create_task(app.run())
 
     try:
         await asyncio.sleep(1)
-        metrics_persister.save.assert_called_with(metrics)
+        mocks.dependencies.metrics_persister.save.assert_called_with(mocks.metrics)
     finally:
         main.cancel()
         await main
