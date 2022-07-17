71d0abcc63423dd4cf1bdd6117fd7e3cc60c7264
Jan Gosmann <jan@hyper-world.de>
2022-07-17 09:50:50 +0200

    Ensure all errors in polling function are logged

--- dmarc_metrics_exporter/imap_queue.py
+++ dmarc_metrics_exporter/imap_queue.py
@@ -42,52 +42,58 @@ class ImapQueue:
         self._poll_task = asyncio.create_task(self._poll_imap(handler))
 
     async def _poll_imap(self, handler: Callable[[Any], Awaitable[None]]):
-        while not self._stop.is_set():
-            logger.debug("Polling IMAP ...")
-            try:
-                async with ImapClient(self.connection, self.timeout_seconds) as client:
-                    for folder in astuple(self.folders):
-                        await client.create_if_not_exists(folder)
+        try:
+            while not self._stop.is_set():
+                logger.debug("Polling IMAP ...")
+                try:
+                    await self._process_new_messages(handler)
+                except (  # pylint: disable=broad-except
+                    asyncio.TimeoutError,
+                    Exception,
+                ):
+                    logger.exception("Error during message processing.")
+                logger.debug(
+                    "Going to sleep for %s seconds until next poll.",
+                    self.poll_interval_seconds,
+                )
+                with contextlib.suppress(asyncio.TimeoutError):
+                    await asyncio.wait_for(
+                        self._stop.wait(), self.poll_interval_seconds
+                    )
+        except Exception:  # pylint: disable=broad-except
+            logger.exception("Error in IMAP queue polling function.")
+            return
 
-                    msg_count = await client.select(self.folders.inbox)
-                    logger.debug("%s messages to fetch.", msg_count)
-                    if msg_count > 0:
-                        fetch_task = asyncio.create_task(
-                            client.fetch(
-                                b"1:" + str(msg_count).encode("ascii"), b"(UID RFC822)"
+    async def _process_new_messages(self, handler: Callable[[Any], Awaitable[None]]):
+        async with ImapClient(self.connection, self.timeout_seconds) as client:
+            for folder in astuple(self.folders):
+                await client.create_if_not_exists(folder)
+
+            msg_count = await client.select(self.folders.inbox)
+            logger.debug("%s messages to fetch.", msg_count)
+            if msg_count > 0:
+                fetch_task = asyncio.create_task(
+                    client.fetch(
+                        b"1:" + str(msg_count).encode("ascii"), b"(UID RFC822)"
+                    )
+                )
+                while not fetch_task.done() or not client.fetched_queue.empty():
+                    uid, msg = self._extract_uid_and_msg(
+                        await client.fetched_queue.get()
+                    )
+                    if uid is not None and msg is not None:
+                        try:
+                            logger.debug("Processing UID %s", uid)
+                            await handler(msg)
+                        except Exception:  # pylint: disable=broad-except
+                            logger.exception(
+                                "Handler for message in IMAP queue failed."
                             )
-                        )
-                        while not fetch_task.done() or not client.fetched_queue.empty():
-                            uid, msg = self._extract_uid_and_msg(
-                                await client.fetched_queue.get()
-                            )
-                            if uid is not None and msg is not None:
-                                try:
-                                    logger.debug("Processing UID %s", uid)
-                                    await handler(msg)
-                                except Exception:  # pylint: disable=broad-except
-                                    logger.exception(
-                                        "Handler for message in IMAP queue failed."
-                                    )
-                                    await client.uid_move_graceful(
-                                        uid, self.folders.error
-                                    )
-                                else:
-                                    logger.debug("foo")
-                                    await client.uid_move_graceful(
-                                        uid, self.folders.done
-                                    )
-                                    logger.debug("bar")
-                        logger.debug("Processed all messages.")
-                        await fetch_task
-            except (asyncio.TimeoutError, Exception):  # pylint: disable=broad-except
-                logger.exception("Error during IMAP queue polling.")
-            logger.debug(
-                "Going to sleep for %s seconds until next poll.",
-                self.poll_interval_seconds,
-            )
-            with contextlib.suppress(asyncio.TimeoutError):
-                await asyncio.wait_for(self._stop.wait(), self.poll_interval_seconds)
+                            await client.uid_move_graceful(uid, self.folders.error)
+                        else:
+                            await client.uid_move_graceful(uid, self.folders.done)
+                logger.debug("Processed all messages.")
+                await fetch_task
 
     @classmethod
     def _extract_uid_and_msg(
